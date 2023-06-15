ae4743da7f9c670c0b88b0d7b64ee5ae4563e03b
kushagraboxmd <kushagraboxmd@gmail.com>
2023-06-15 14:47:24 +0530

    signup changes

--- app/hooks/useAuth.jsx
+++ app/hooks/useAuth.jsx
@@ -4,10 +4,17 @@ import dataType from "../constants/dataType";
 
 export default useAuth = () => {
   const { userToken: auth, setAuth: setAuth } = useContext(TokenContext);
+  let role = null;
+  let token = null;
+  if (auth) {
+    role = typeof auth.role === dataType.string ? auth.role : auth.role.name;
+
+    token = auth.token;
+  }
   return {
     auth,
     setAuth,
-    role: typeof auth.role === dataType.string ? auth.role : auth.role.name,
-    token: auth.token,
+    role,
+    token,
   };
 };
